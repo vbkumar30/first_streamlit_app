525ecdbd6985b47d0c0b4ebffc7bdff5ca7c076e
vbkumar30 <130329670+vbkumar30@users.noreply.github.com>
2023-04-10 17:09:56 +0900

    Update streamlit_app.py
    After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 , Blueberry & Oatmeal')
 streamlit.text('kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-range eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
