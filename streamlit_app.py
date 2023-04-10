1de789bed27c836fbd2dc1ebef8c2b4e2a1f2b6d
vbkumar30 <130329670+vbkumar30@users.noreply.github.com>
2023-04-10 13:16:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 , Blueberry & Oatmeal')
 streamlit.text('kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-range eggs')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
