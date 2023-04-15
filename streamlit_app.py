2fe22a4361df7855d46f6ac3514fffe9d037f9b9
vbkumar30 <130329670+vbkumar30@users.noreply.github.com>
2023-04-15 19:57:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.text(fruityvice_response.json())
 # take the json version of the data and normalie it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
